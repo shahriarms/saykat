36701667ba175dadf8dee27f472934803fd6254a
Shahriar MS <shahriar.ms1k@gmail.com>
2025-09-24 08:04:12 +0000

    Error: ./src/components/stock-status-card.tsx:4:1 [31m[1mModule not fo

--- src/components/stock-status-card.tsx
+++ src/components/stock-status-card.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import * as React from 'use-memo';
+import * as React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import {
