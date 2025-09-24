13106e79a3c0ef724b3dfec7b5c97df872bdf76a
Shahriar MS <shahriar.ms1k@gmail.com>
2025-09-24 20:14:35 +0000

    web app ar shob jaygay jea shadow use hoisea aktu komano uchit

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-2xl",
+      "rounded-lg border bg-card text-card-foreground shadow-lg",
       className
     )}
     {...props}
